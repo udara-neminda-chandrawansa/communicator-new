85adbb2ac8946dd298aa678abc340764f06da832
Udara Neminda <udaraneminda@gmail.com>
2024-02-14 11:39:36 +0530

    test-commit

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -30,7 +30,7 @@ function Nav() {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/">Home</a>
+              <a className="nav-link active" aria-current="page" href="/unccom">Home</a>
             </li>
             <li className="nav-item">
               <a className="nav-link active" href={loginLink}>Login</a>
